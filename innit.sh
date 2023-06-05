e00aa392efc99c29c7097764b9485d3e792604a9
bieniucieniu <mikibien@outlook.com>
2023-06-05 16:13:37 +0200

    add stow

--- innit.sh
+++ innit.sh
@@ -14,13 +14,17 @@ yay -Sy --needed nerd-fonts-meta
 #oh my zsh
 pacman -Sy --needed zsh curl git && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-if [ -d "${HOME}/.zshrc"]
-then
-    source ${HOME}/.zshrc
-fi
+source ${HOME}/.zshrc
 
-if [ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins" ]
-then
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+stow ${SCRIPT_DIR}/alacritty
+stow ${SCRIPT_DIR}/background
+stow ${SCRIPT_DIR}/dunst
+stow ${SCRIPT_DIR}/eww
+stow ${SCRIPT_DIR}/hypr
+stow ${SCRIPT_DIR}/rofi
+stow ${SCRIPT_DIR}/swaylock
