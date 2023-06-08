c53fc31b6f95321e9fe2d0d3aaa511b18b4daada
bieniucieniu <mikibien@outlook.com>
2023-06-08 13:02:41 +0000

    reset configs

--- innit.sh
+++ innit.sh
@@ -6,7 +6,7 @@ then
     makepkg -si
 fi
 
-yay -Sy --needed hyprland swaybg swaylock-effects swayidle grim slurp wl-clipboard dunst xdg-user-dirs rofi viewnior eww-wayland starship thunar thunar-volman thunar-archive-plugin gvfs file-roller gsettings-desktop-schemas lxappearance Kripton Qogir-cursors playerctl polkit dbus brightnessctl zsh alacritty networkmanageo bluez bluez-utils pulseaudio pulseaudio-bluetooth pipewire blueman nvim github-cli google-chrome
+yay -Sy --needed hyprland wl-clipboard rofi eww-wayland zsh alacritty networkmanageo bluez bluez-utils pulseaudio pulseaudio-bluetooth pipewire blueman nvim github-cli google-chrome
 
 #all nerd-fonts
 yay -Sy --needed nerd-fonts-meta
@@ -22,9 +22,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 stow ${SCRIPT_DIR}/alacritty
-stow ${SCRIPT_DIR}/background
-stow ${SCRIPT_DIR}/dunst
-stow ${SCRIPT_DIR}/eww
+# stow ${SCRIPT_DIR}/background
+# stow ${SCRIPT_DIR}/dunst
+# stow ${SCRIPT_DIR}/eww
 stow ${SCRIPT_DIR}/hypr
-stow ${SCRIPT_DIR}/rofi
-stow ${SCRIPT_DIR}/swaylock
+# stow ${SCRIPT_DIR}/rofi
