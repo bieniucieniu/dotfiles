6ad14fe25bf99b9869fef82d9e3c7c42bb65f782
bieniucieniu <mikibien@outlook.com>
2024-03-21 14:23:31 +0100

    change zsh to fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,22 @@
+set -g fish_greeting
+
+if status --is-login
+end
+if status is-interactive
+end
+
+
+zoxide init fish | source
+
+
+
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+export PATH="$PATH:$HOME/.turso:$PATH"
+export PATH="$PATH:$HOME/go/bin:$PATH"
+export PATH="$PATH:$HOME/.cargo/env"
+
+
+
+
+# Load Angular CLI autocompletion.
