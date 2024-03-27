92685a2abb5a9fbfb7b3a0f495f6da2806e06964
bieniucieniu <mikibien@outlook.com>
2024-03-27 12:24:24 +0100

    fix path appending

--- fish/.config/fish/conf.d/path.fish
+++ fish/.config/fish/conf.d/path.fish
@@ -1,4 +1,6 @@
 
-export PATH="$HOME/.bun/bin:$PATH"
-export PATH="$PATH:$HOME/.turso:$PATH"
-export PATH="$PATH:$HOME/.cargo/env"
+fish_add_path "$HOME/go/bin"
+fish_add_path "$HOME/.bun/bin"
+fish_add_path "$HOME/.turso"
+fish_add_path "$HOME/.cargo/env"
+fish_add_path "$HOME/.cargo/bin"
