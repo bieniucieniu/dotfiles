091c8434e5d875feb95a6ba970983049edbbdaf4
bieniu <mikibien@outlook.com>
2024-12-21 09:30:46 +0100

    remove format on save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,10 +4,6 @@ return {
   opts = function()
     return {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
       formatters_by_ft = {
         lua = { "stylua" },
         typescriptreact = { "biome-check" },
